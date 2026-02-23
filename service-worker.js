3d025a36c79a29fb1a5edd52069387a7b9f29e3d
allanbarbosasza-dev <allanbarbosasza@gmail.com>
2026-02-23 03:50:08 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'orcamentos-v1';
+const ASSETS = [
+  './Orcamentov18.html',
+  './manifest.json'
+];
+
+// Instala e faz cache dos arquivos principais
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Ativa e remove caches antigos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Serve do cache quando offline, tenta rede quando online
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).catch(() => cached);
+    })
+  );
+});
